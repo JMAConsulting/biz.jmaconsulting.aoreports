086fee35d484a021a55c9d3407ce04526cebeb95
Monish Deb <deb.monish@gmail.com>
2018-12-03 23:55:07 +0530

    minor fixes

--- CRM/AOReports/Form/Report/ParentFeedback.php
+++ CRM/AOReports/Form/Report/ParentFeedback.php
@@ -267,6 +267,7 @@ class CRM_AOReports_Form_Report_ParentFeedback extends CRM_Report_Form {
     CRM_Core_Resources::singleton()->addScript(
     "CRM.$(function($) {
       $('.report-layout thead').hide();
+      $('.criteria-group span.crm-frozen-field').hide();
     });"
   );
   }
