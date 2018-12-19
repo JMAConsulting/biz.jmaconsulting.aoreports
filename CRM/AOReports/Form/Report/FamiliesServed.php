75bc971f2bca0dcd9734e36fca8e2b6e7b81e789
Monish Deb <deb.monish@gmail.com>
2018-12-19 16:21:32 +0530

    minor fix

--- CRM/AOReports/Form/Report/FamiliesServed.php
+++ CRM/AOReports/Form/Report/FamiliesServed.php
@@ -106,8 +106,8 @@ class CRM_AOReports_Form_Report_FamiliesServed extends CRM_Report_Form {
   function from() {
     $tableName = E::getNewChildContactTableName();
     $this->_from = " FROM civicrm_contact {$this->_aliases['civicrm_contact']}
-      LEFT JOIN {$tableName} temp ON temp.new_child_id = {$this->_aliases['civicrm_contact']}.id
-      LEFT JOIN civicrm_value_donation_cust_2 lang ON lang.entity_id = temp.new_child_id AND lang.language_10 IS NOT NULL
+      INNER JOIN {$tableName} temp ON temp.new_child_id = {$this->_aliases['civicrm_contact']}.id
+      INNER JOIN civicrm_value_donation_cust_2 lang ON lang.entity_id = temp.new_child_id AND lang.language_10 IS NOT NULL
     ";
   }
 
