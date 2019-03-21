f0543f5f6a57d9ddcc90231e2af98113703865ad
Monish Deb <deb.monish@gmail.com>
2019-03-21 20:14:58 +0530

    Add Service Navigation Report

---
+++ CRM/AOReports/Form/Report/ServiceNavigation.php
@@ -0,0 +1,68 @@
+<?php
+use CRM_AOReports_Utils as E;
+
+class CRM_AOReports_Form_Report_ServiceNavigation extends CRM_AOReports_Form_Report_FamiliesServed {
+
+  function __construct() {
+    parent::__construct();
+    $this->_columns['civicrm_contact']['filters']['language_10'] = array(
+      'name' => 'language_10',
+      'title' => 'Language',
+      'operator' => 'like',
+      'type' => CRM_Report_Form::OP_STRING,
+    );
+    $this->_columns['civicrm_contact']['fields']['family_count'] = array(
+      'title' => ts('&nbsp;&nbsp;&nbsp;DATA ELEMENT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'),
+      'required' => TRUE,
+      'dbAlias' => "temp.region",
+    );
+  }
+
+  function from() {
+    $acgtivityTypeID = CRM_Core_PseudoConstant::getKey('CRM_Activity_BAO_Activity', 'activity_type_id', 'Service Navigation Provision');
+    $tableName = E::getSNPActivityTableName($acgtivityTypeID);
+    $this->_from = " FROM civicrm_contact {$this->_aliases['civicrm_contact']}
+      INNER JOIN {$tableName} temp ON temp.parent_id = {$this->_aliases['civicrm_contact']}.id
+      INNER JOIN civicrm_value_donation_cust_2 lang ON lang.entity_id = temp.parent_id AND lang.language_10 IS NOT NULL
+    ";
+  }
+
+  function groupBy() {
+    $this->_groupBy = "GROUP BY temp.region, QUARTER(temp.dof)";
+  }
+
+  function alterDisplay(&$rows) {
+    $originalSQL = $this->buildQuery(TRUE);
+    $newRows = [];
+    $defaultYear = '';
+
+    $regions = CRM_Core_OptionGroup::values('region');
+    foreach ($regions as $value => $name) {
+      $newRows[$value] = [
+        'civicrm_contact_total' => 1,
+        'civicrm_contact_family_count' => ts('SNPNFS %1 Count of unique parents/caregiver contacts who received SNP services', [1 => $name]),
+        'civicrm_contact_year' => '',
+        'civicrm_contact_quarter' => NULL,
+        'civicrm_contact_q1' => 0,
+        'civicrm_contact_q2' => 0,
+        'civicrm_contact_q3' => 0,
+        'civicrm_contact_q4' => 0,
+      ];
+    }
+
+    foreach ($newRows as $key => $row) {
+      foreach ($rows as &$row) {
+        if (strstr($row['civicrm_contact_family_count'], $key)) {
+          $newRows[$key]['civicrm_contact_quarter'] = $row['civicrm_contact_quarter'];
+          $newRows[$key]["civicrm_contact_q{$row['civicrm_contact_quarter']}"] = $row['civicrm_contact_total'];
+        }
+      }
+    }
+
+    unset($this->_columnHeaders["civicrm_contact_total"]);
+    unset($this->_columnHeaders["civicrm_contact_quarter"]);
+
+    $rows = $newRows;
+  }
+
+}
