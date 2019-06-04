09ffd5012ce14916e5cf4d3783443ecd3edd1f76
Monish Deb <deb.monish@gmail.com>
2019-06-04 20:29:16 +0530

    Add Extended Batch Detail Report

--- CRM/AOReports/Form/Report/ExtendedBatchDetail.php
+++ CRM/AOReports/Form/Report/ExtendedBatchDetail.php
@@ -1,44 +1,7 @@
 <?php
 
 class CRM_AOReports_Form_Report_ExtendedBatchDetail extends CRM_Report_Form_Contribute_BatchDetail {
-  public function __construct() {
-    parent::__construct();
-    $this->_columns['civicrm_chapter_entity']['fields']['chapter_code_from'] = array(
-      'name' => 'chapter_code_from',
-      'title' => ts('Chapter Code - Credit'),
-      'dbAlias' => 'CONCAT(ce_from.chapter_code, " ", covc_from.label)',
-      'is_default' => TRUE,
-    );
-    $this->_columns['civicrm_chapter_entity']['fields']['chapter_code_to'] = array(
-      'name' => 'chapter_code_to',
-      'title' => ts('Chapter Code - Debit'),
-      'dbAlias' => 'CONCAT(ce_to.chapter_code, " ", covc_to.label)',
-      'is_default' => TRUE,
-    );
-    $this->_columns['civicrm_chapter_entity']['fields']['fund_code_from'] = array(
-      'name' => 'fund_code_from',
-      'title' => ts('Fund Code - Credit'),
-      'dbAlias' => 'CONCAT(ce_from.fund_code, " ", covf_from.label)',
-      'is_default' => TRUE,
-    );
-    $this->_columns['civicrm_chapter_entity']['fields']['fund_code_to'] = array(
-      'name' => 'fund_code_to',
-      'title' => ts('Fund Code - Debit'),
-      'dbAlias' => 'CONCAT(ce_to.fund_code, " ", covf_to.label)',
-      'is_default' => TRUE,
-    );
-    $this->_columns['civicrm_chapter_entity']['fields']['fund_id'] = array(
-      'name' => 'fund_id',
-      'title' => ts('Fund ID'),
-      'dbAlias' => 'CASE
-        WHEN financial_trxn_civireport.from_financial_account_id IS NOT NULL
-        THEN  CONCAT(financial_account_civireport_credit_1.accounting_code, "-", ce_to.chapter_code)
-        ELSE  CONCAT(financial_account_civireport_credit_2.accounting_code, "-", ce_to.chapter_code)
-        END',
-      'is_default' => TRUE,
-    );
-  }
-
+    
   public function groupBy() {
     $groupBy = [
       "{$this->_aliases['civicrm_batch']}.id",
