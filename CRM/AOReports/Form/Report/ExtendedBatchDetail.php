33f9638dcd4596198afb9c073d360bac6b2a0b9d
Monish Deb <deb.monish@gmail.com>
2019-06-04 23:04:17 +0530

    minor fix

--- CRM/AOReports/Form/Report/ExtendedBatchDetail.php
+++ CRM/AOReports/Form/Report/ExtendedBatchDetail.php
@@ -7,6 +7,86 @@ class CRM_AOReports_Form_Report_ExtendedBatchDetail extends CRM_Report_Form_Cont
     unset($this->_columns['civicrm_contribution']['fields']['invoice_id']);
   }
 
+  public function select() {
+    $select = array();
+
+    $this->_columnHeaders = array();
+    foreach ($this->_columns as $tableName => $table) {
+      if (array_key_exists('fields', $table)) {
+        foreach ($table['fields'] as $fieldName => $field) {
+          if (!empty($field['required']) ||
+            !empty($this->_params['fields'][$fieldName])
+          ) {
+            switch ($fieldName) {
+              case 'credit_accounting_code':
+              case 'credit_name':
+                $select[] = " CASE
+                            WHEN {$this->_aliases['civicrm_financial_trxn']}.from_financial_account_id IS NOT NULL
+                            THEN  {$this->_aliases['civicrm_financial_account']}_credit_1.{$field['name']}
+                            ELSE  {$this->_aliases['civicrm_financial_account']}_credit_2.{$field['name']}
+                            END AS civicrm_financial_account_{$fieldName} ";
+                break;
+
+              case 'amount':
+              case 'debit_amount':
+                $select[] = " CASE
+                            WHEN  ceft1.entity_id IS NOT NULL
+                            THEN ceft1.amount
+                            ELSE ceft.amount
+                            END AS civicrm_entity_financial_trxn_{$fieldName} ";
+                break;
+
+              case 'credit_contact_id':
+                $select[] = " CASE
+                            WHEN {$this->_aliases['civicrm_financial_trxn']}.from_financial_account_id IS NOT NULL
+                            THEN  credit_contact_1.{$field['name']}
+                            ELSE  credit_contact_2.{$field['name']}
+                            END AS civicrm_financial_account_{$fieldName} ";
+                break;
+
+              default:
+                $select[] = "{$field['dbAlias']} as {$tableName}_{$fieldName}";
+                break;
+            }
+            if (!$field['no_display']) {
+              $this->_columnHeaders["{$tableName}_{$fieldName}"]['title'] = $field['title'];
+              $this->_columnHeaders["{$tableName}_{$fieldName}"]['type'] = CRM_Utils_Array::value('type', $field);
+            }
+          }
+        }
+      }
+    }
+    // Rearrange select clause
+    $order = [
+      'civicrm_batch_batch_id',
+      'civicrm_contribution_invoice_id',
+      'civicrm_contribution_contact_id',
+      'civicrm_financial_trxn_payment_id',
+      'civicrm_financial_trxn_trxn_date',
+      'civicrm_financial_account_debit_accounting_code',
+      'civicrm_financial_account_debit_name',
+      'civicrm_financial_account_debit_account_type_code',
+      'civicrm_financial_trxn_total_amount',
+      'civicrm_financial_trxn_trxn_id',
+      'civicrm_entity_financial_trxn_debit_amount',
+      'civicrm_financial_trxn_payment_instrument_id',
+      'civicrm_financial_trxn_check_number',
+      'civicrm_contribution_source',
+      'civicrm_financial_trxn_currency',
+      'civicrm_financial_trxn_status_id',
+      'civicrm_entity_financial_trxn_amount',
+      'civicrm_financial_account_credit_accounting_code',
+      'civicrm_financial_account_credit_name',
+      'civicrm_financial_account_credit_account_type_code',
+      'civicrm_financial_item_description',
+    ];
+    $this->_columnHeaders = array_replace(array_flip($order), $this->_columnHeaders);
+    unset($this->_columnHeaders['civicrm_contribution_contribution_id']);
+    $this->_selectClauses = $select;
+
+    $this->_select = 'SELECT ' . implode(', ', $select) . ' ';
+  }
+
   public function groupBy() {
     $groupBy = [
       "{$this->_aliases['civicrm_batch']}.id",
