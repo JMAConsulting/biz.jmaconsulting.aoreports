1ba9d2b1fe4a38010f44ba80b86e858dc54960b1
Monish Deb <deb.monish@gmail.com>
2019-07-03 16:02:02 +0530

    minor fix

--- CRM/AOReports/Form/Report/ExtendServiceNavigation.php
+++ CRM/AOReports/Form/Report/ExtendServiceNavigation.php
@@ -8,8 +8,19 @@ class CRM_AOReports_Form_Report_ExtendServiceNavigation extends CRM_AOReports_Fo
     $this->_columns['civicrm_contact']['fields']['time_diff'] = array(
       'title' => ts('Time Diff'),
       'required' => TRUE,
-      'no_display' => TRUE,
       'dbAlias' => "SUM(temp.timediff)",
+    );
+
+    $this->_columns['civicrm_contact']['filters']['time'] = array(
+      'name' => 'time',
+      'dbAlias' => "1",
+      'title' => 'Waiting Time',
+      'type' => CRM_Utils_Type::T_INT,
+      'operatorType' => CRM_Report_Form::OP_SELECT,
+      'options' => [
+        1 => ts('In Days'),
+        2 => ts('In Hours'),
+      ],
     );
   }
 
@@ -19,6 +30,49 @@ class CRM_AOReports_Form_Report_ExtendServiceNavigation extends CRM_AOReports_Fo
       INNER JOIN {$tableName} temp ON temp.parent_id = {$this->_aliases['civicrm_contact']}.id
       INNER JOIN civicrm_value_donation_cust_2 lang ON lang.entity_id = temp.parent_id AND lang.language_10 IS NOT NULL
     ";
+  }
+
+  function where() {
+    $clauses = array();
+    foreach ($this->_columns as $tableName => $table) {
+      if (array_key_exists('filters', $table)) {
+        foreach ($table['filters'] as $fieldName => $field) {
+          $clause = NULL;
+          if (in_array($fieldName, ['activity_type', 'time'])) {
+            continue;
+          }
+          if (CRM_Utils_Array::value('operatorType', $field) & CRM_Utils_Type::T_DATE) {
+            $relative = CRM_Utils_Array::value("{$fieldName}_relative", $this->_params);
+            $from     = CRM_Utils_Array::value("{$fieldName}_from", $this->_params);
+            $to       = CRM_Utils_Array::value("{$fieldName}_to", $this->_params);
+
+            $this->_dateClause = $clause = $this->dateClause($field['name'], $relative, $from, $to, $field['type']);
+          }
+          else {
+            $op = CRM_Utils_Array::value("{$fieldName}_op", $this->_params);
+            if ($op) {
+              $clause = $this->whereClause($field,
+                $op,
+                CRM_Utils_Array::value("{$fieldName}_value", $this->_params),
+                CRM_Utils_Array::value("{$fieldName}_min", $this->_params),
+                CRM_Utils_Array::value("{$fieldName}_max", $this->_params)
+              );
+            }
+          }
+
+          if (!empty($clause)) {
+            $clauses[] = $clause;
+          }
+        }
+      }
+    }
+
+    if (empty($clauses)) {
+      $this->_where = "WHERE ( 1 ) ";
+    }
+    else {
+      $this->_where = "WHERE " . implode(' AND ', $clauses);
+    }
   }
 
   function alterDisplay(&$rows) {
@@ -45,7 +99,10 @@ class CRM_AOReports_Form_Report_ExtendServiceNavigation extends CRM_AOReports_Fo
       foreach ($rows as &$row) {
         if (strstr($row['civicrm_contact_family_count'], $key)) {
           $newRows[$key]['civicrm_contact_quarter'] = $row['civicrm_contact_quarter'];
-          $newRows[$key]["civicrm_contact_q{$row['civicrm_contact_quarter']}"] = $row['civicrm_contact_total'] == 0 ? 0 : round(($row['civicrm_contact_time_diff'] / $row['civicrm_contact_total']), 2);
+          $newRows[$key]["civicrm_contact_q{$row['civicrm_contact_quarter']}"] = $row['civicrm_contact_total'] == 0 ? 0 : round(($row['civicrm_contact_time_diff'] / $row['civicrm_contact_total']));
+          if ($this->_params['time_value'] == 2) {
+            $newRows[$key]["civicrm_contact_q{$row['civicrm_contact_quarter']}"] *= 24;
+          }
         }
       }
     }
@@ -56,6 +113,7 @@ class CRM_AOReports_Form_Report_ExtendServiceNavigation extends CRM_AOReports_Fo
 
     unset($this->_columnHeaders["civicrm_contact_total"]);
     unset($this->_columnHeaders["civicrm_contact_quarter"]);
+    unset($this->_columnHeaders["civicrm_contact_time_diff"]);
 
     $rows = $newRows;
   }
