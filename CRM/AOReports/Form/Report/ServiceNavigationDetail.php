142902729678d6441c824a922c21a5362e9881e8
Monish Deb <deb.monish@gmail.com>
2020-02-07 04:42:56 +0530

    AOS-333 Add address columns in no-service-contact report

--- CRM/AOReports/Form/Report/ServiceNavigationDetail.php
+++ CRM/AOReports/Form/Report/ServiceNavigationDetail.php
@@ -59,7 +59,7 @@ class CRM_AOReports_Form_Report_ServiceNavigationDetail extends CRM_Report_Form 
             ),
           ),
         ),
-      );
+      ) + $this->getAddressColumns(array('group_bys' => FALSE));
       parent::__construct();
     }
 
@@ -95,6 +95,7 @@ class CRM_AOReports_Form_Report_ServiceNavigationDetail extends CRM_Report_Form 
       $tableName = E::getSNPActivityTableName($this->_params['activity_type_value'], $this, $this->_params['status_id_value'], $this->_params['status_id_op'], $tempTableWhere);
       $this->_from = " FROM civicrm_contact {$this->_aliases['civicrm_contact']}
         INNER JOIN {$tableName} temp ON temp.parent_id = {$this->_aliases['civicrm_contact']}.id ";
+      $this->joinAddressFromContact();
     }
 
     function groupBy() {
