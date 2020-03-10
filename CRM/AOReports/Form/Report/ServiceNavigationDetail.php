79b67b3d0c7cc4e03d6d94f457a6b041ffb71a65
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2020-03-10 19:53:22 +0530

    Fixed typo in URL

--- CRM/AOReports/Form/Report/ServiceNavigationDetail.php
+++ CRM/AOReports/Form/Report/ServiceNavigationDetail.php
@@ -158,7 +158,7 @@ class CRM_AOReports_Form_Report_ServiceNavigationDetail extends CRM_Report_Form 
         4 => 'Oct-Dec',
       ];
       foreach ($rows as $rowNum => $row) {
-        $rows[$rowNum]['civicrm_contact_contact_name'] = sprintf("<a href='%s'>%s</a>", CRM_Utils_System::url('civicrm/contact/view', 'reset=1&id=' . $row['civicrm_contact_contact_id']), $rows[$rowNum]['civicrm_contact_contact_name']);
+        $rows[$rowNum]['civicrm_contact_contact_name'] = sprintf("<a href='%s'>%s</a>", CRM_Utils_System::url('civicrm/contact/view', 'reset=1&cid=' . $row['civicrm_contact_contact_id']), $rows[$rowNum]['civicrm_contact_contact_name']);
         $rows[$rowNum]['civicrm_contact_quarter'] = CRM_Utils_Array::value($rows[$rowNum]['civicrm_contact_quarter'], $quarters);
       }
     }
